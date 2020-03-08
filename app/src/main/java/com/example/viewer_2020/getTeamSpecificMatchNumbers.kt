3c910dc24cd617fb98c11557a981fcb57764d3cc
Kathy Li <55221809+kathyli05@users.noreply.github.com>
2020-03-08 15:33:48 -0700

    Sort our match schedule with correct match number order (#52)
    * Fix sort
    
    * Delete comment and todo
    
    * Update getTeamSpecificMatchNumbers.kt

--- app/src/main/java/com/example/viewer_2020/getTeamSpecificMatchNumbers.kt
+++ app/src/main/java/com/example/viewer_2020/getTeamSpecificMatchNumbers.kt
@@ -13,15 +13,22 @@ import com.example.viewer_2020.constants.Constants
 // Returns a filtered list of matches for the given team number.
 fun getTeamSpecificMatchNumbers(teamNumber: String): List<String> {
     val matches = mutableListOf<String>()
+    val sortedMatches: ArrayList<Int> = ArrayList()
+    val sortedMatchListString: ArrayList<String> = ArrayList()
     for (`object` in getDirectField(MainViewerActivity.databaseReference!!.processed,
         Constants.PROCESSED_OBJECT.CALCULATED_OBJECTIVE_TEAM_IN_MATCH.value)
             as Array<*>) {
         if (getDirectField(`object`!!, "team_number").toString() == teamNumber) {
             if (getDirectField(`object`, "match_number").toString() !in matches) {
                 matches.add(getDirectField(`object`, "match_number").toString())
+                sortedMatches.add(getDirectField(`object`, "match_number").toString().toInt())
             }
         }
     }
-    return matches
+    sortedMatches.sort()
+    sortedMatches.forEach {
+        sortedMatchListString.add(it.toString())
+    }
+    return sortedMatchListString
 }
 
